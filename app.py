7a3ae38954446cbc8bf1433fe6dadad75cab822a
aqeelcharania <aacharan@lakeheadu.ca>
2020-11-16 15:57:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 from typing import Text
 from flask import Flask, render_template , request ,flash
 from model import read
+import os
+import argparse
+import subprocess
+import logging
+from flask import Flask, request, jsonify, render_template, url_for, flash, redirect
+from werkzeug.exceptions import abort
+from flask_cors import CORS
+
+import signal
+import datetime
+import sys
+import shlex
+
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
- return render_template('index.html')
+    
+ return render_template('base2.html')
+
+@app.route('/about')
+def about():
+ return render_template('group.html')
 
 #inference
 @app.route('/inference', methods=['POST', 'GET'])
@@ -20,11 +39,11 @@ def inference():
         children = request.form['kid']
 
         if not sex:
-            flash('enter sex male or female')
+            flash('enter male or female')
         else:
             expense = read(sex,smoker,region,age, bmi,children)
-            return render_template('/inference.html',expense = expense)
-    return render_template('/inference.html')
+            return render_template('inter.html',expense = expense)
+    return render_template('inter.html')
 
 if __name__ == "__main__":    
       app.run(debug=True)
