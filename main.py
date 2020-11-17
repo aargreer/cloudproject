24077eacf87b1e3fbf18b1027a455746ec18563f
aqeelcharania <aacharan@lakeheadu.ca>
2020-11-17 03:07:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -31,4 +31,4 @@ def inference():
     return render_template('inference.html')
 
 if __name__ == "__main__":    
-      app.run(debug=True, host='127.0.0.1', port=5000)
+      app.run(host='0.0.0.0', port=8080, debug=True)
